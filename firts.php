4384e66e083e36929b088bef2c54c3545c0f331b
kirikotk <kirikotk@greeny.cs.tlu.ee>
2016-09-08 15:17:49 +0300

    first.php

---
+++ firts.php
@@ -0,0 +1,14 @@
+<?php
+
+    $firstName = "Kirill";
+	$lastName = "Kotkas";
+	
+	// käsk echo trükib välja muutuja väärtusi
+	
+	echo $firstName;
+	echo "</p><p>";
+	echo $lastName;
+	echo "</p><p>";
+	echo $firstName." ".$lastName
+
+?>
